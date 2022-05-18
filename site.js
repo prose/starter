ffe67ac197dffb91dce50b40a8ad6446d84bb0b8
Scott Kilts <sjkilts@users.noreply.github.com>
2022-05-18 02:15:13 -0700

    force links from external domains to background

--- site.js
+++ site.js
@@ -1,3 +1,7 @@
 ---
 ---
 ;{% include js/jquery.min.js %}
+
+Array.from(javascript.links)
+    .filter(link => link.hostname != window.location.hostname)
+    .forEach(link => link.target = '_blank');

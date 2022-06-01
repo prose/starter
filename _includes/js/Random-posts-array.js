dabc2c480609623a1c19664cebb711583d9a9030
Scott Kilts <sjkilts@users.noreply.github.com>
2022-05-31 21:21:45 -0700

    Random-posts-array.js cleanup

--- _includes/js/Random-posts-array.js
+++ _includes/js/Random-posts-array.js
@@ -1,11 +1,11 @@
 <script type="text/javascript">
 
-    var postsHREF = [{% for post in site.posts %}"{{ post.url }}"{% unless forloop.last %},{% endunless %}{% endfor %}];
+    var postsHREF = [{% for post in site.posts limit:5 -%}"{{ post.url }}"{% unless forloop.last %},{% endunless %}{%- endfor %}];
 
-    var postsTitle = [{% for post in site.posts %}"{{ post.title }}"{% unless forloop.last %},{% endunless %}{% endfor %}];
+    var postsTitle = [{% for post in site.posts limit:5 -%}"{{ post.title }}"{% unless forloop.last %},{% endunless %}{%- endfor %}];
     
-    var postsDate = [{% for post in site.posts %}"{{ post.date | date:"%b %f %Y" }}"{% unless forloop.last %},{% endunless %}{% endfor %}];
+    var postsDate = [{% for post in site.posts limit:5 -%}"{{ post.date | date:"%b %f %Y" }}"{% unless forloop.last %},{% endunless %}{%- endfor %}];
     
-    var postsExcerpt = [{% for post in site.posts %}"{{ post.excerpt }}"{% unless forloop.last %},{% endunless %}{% endfor %}];
+    var postsExcerpt = [{% for post in site.posts limit:5 -%}"{{ post.excerpt }}"{% unless forloop.last %},{% endunless %}{%- endfor %}];
 
 </script>

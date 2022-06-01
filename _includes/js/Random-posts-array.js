660eeec8523f02d9a21ab0f1db0535559c93c831
Scott Kilts <sjkilts@users.noreply.github.com>
2022-06-01 16:19:50 -0700

    cleanup

--- _includes/js/Random-posts-array.js
+++ _includes/js/Random-posts-array.js
@@ -4,6 +4,6 @@
 
     var postsTitle = [{% for post in site.posts limit:5 -%}"{{ post.title }}"{% unless forloop.last %},{% endunless %}{%- endfor %}];
     
-    var postsExcerpt = [{% for item in site.categories.post limit:5 -%}"{{ item.excerpt }}"{% unless forloop.last %},{% endunless %}{%- endfor %}];
+    var postsExcerpt = [{% for post in site.posts limit:5 -%}"{{ post.excerpt }}"{% unless forloop.last %},{% endunless %}{%- endfor %}];
 
 </script>

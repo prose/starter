178977a982a883bfde1e23853ebd301e8e3b823d
Scott Kilts <sjkilts@users.noreply.github.com>
2022-05-31 20:10:17 -0700

    random post experiment

---
+++ _includes/js/Random-post.js
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+
+    var randomIndexUsed = [];
+    var counter = 0;
+    var numberOfPosts = 1;
+
+    while (counter < numberOfPosts)
+    {
+        var randomIndex;
+        var postHREF;
+        var postTitle;
+
+        randomIndex = Math.floor(Math.random() * postsHREF.length);
+
+        if (randomIndexUsed.indexOf(randomIndex) == "-1")
+        {
+            postHREF = postsHREF[randomIndex];
+            postTitle = postsTitle[randomIndex];
+
+            if (counter == (numberOfPosts - 1))
+            {
+                document.write('<p><a href="' + postHREF + '">' + postTitle + '</a></p>');
+            }
+            else
+            {
+                document.write('<p><a href="' + postHREF + '">' + postTitle + '</a></p><hr />');
+            }
+
+            randomIndexUsed.push(randomIndex);
+
+            counter++;
+        }
+    }
+
+</script>
---
+++ _includes/js/Random-posts-array.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+
+    var postsHREF = [{% for post in site.posts %}"{{ post.url }}"{% unless forloop.last %},{% endunless %}{% endfor %}];
+
+    var postsTitle = [{% for post in site.posts %}"{{ post.title }}"{% unless forloop.last %},{% endunless %}{% endfor %}];
+
+</script>
